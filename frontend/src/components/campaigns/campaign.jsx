64bd40c0b40dbfbb731f4e7892c3123b6e0ef7a3
Calvin Lee <clee421@gmail.com>
2017-10-20 19:22:04 -0400

    Change campaign card structure

--- frontend/src/components/campaigns/campaign.jsx
+++ frontend/src/components/campaigns/campaign.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Grid, Card, Icon } from 'semantic-ui-react';
+import { Grid, Card, Image, Icon } from 'semantic-ui-react';
 import Volunteer from '../volunteers/volunteer';
 import DefaultImage from '../../assets/images/default.jpg';
 
@@ -10,23 +10,31 @@ class Campaign extends React.Component {
       <Volunteer volunteer={v} key={idx} />
     ));
 
-    const extra = (
-      <a>
-        <Icon name='users' />
-        {`${volunteerList.length} volunteers!`}
-      </a>
-    );
-
     return (
       <Grid.Row>
         <Grid.Column width={6}>
-          <Card
-            image={DefaultImage}
-            header={this.props.campaign.candidate}
-            meta={this.props.campaign.state}
-            description='hendrerit gravida rutrum quisque non tellus orci ac auctor augue mauris augue neque gravida in fermentum et sollicitudin ac orci phasellus egestas tellus rutrum tellus pellentesque eu tincidunt tortor aliquam nulla facilisi cras fermentum odio eu feugiat pretium nibh ipsum consequat nisl vel pretium lectus quam id leo in vitae'
-            extra={extra}
-          />
+          <Card>
+          <Image src={DefaultImage} />
+          <Card.Content>
+            <Card.Header textAlign='left'>
+              <h3>
+                {this.props.campaign.candidate}
+              </h3>
+            </Card.Header>
+            <Card.Meta textAlign='left'>
+              {this.props.campaign.state}
+            </Card.Meta>
+            <Card.Description textAlign='left'>
+              hendrerit gravida rutrum quisque non tellus orci ac auctor augue mauris augue neque gravida in fermentum et sollicitudin ac orci phasellus egestas tellus rutrum tellus pellentesque eu tincidunt tortor aliquam nulla facilisi cras fermentum odio eu feugiat pretium nibh ipsum consequat nisl vel pretium lectus quam id leo in vitae
+            </Card.Description>
+          </Card.Content>
+          <Card.Content textAlign='left' extra>
+            <a>
+              <Icon name='users' />
+              {`${volunteerList.length} volunteers!`}
+            </a>
+          </Card.Content>
+          </Card>
         </Grid.Column>
         <Grid.Column width={6}>
           {volunteerList}
