6d0a8b858cc31eb6bd5a4efb7432790d79fcf0e6
Calvin Lee <clee421@gmail.com>
2017-10-22 21:00:38 -0400

    Remove not needed files, add seeds

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -2,8 +2,7 @@ import React, { Component } from 'react';
 import HeaderNav from './layout/header';
 import PageContent from './layout/page_content';
 
-import 'semantic-ui-css/semantic.min.css';  
-import '../css/app.css';
+import 'semantic-ui-css/semantic.min.css';
 
 class App extends Component {
   render() {
