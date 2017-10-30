837c7db98a598ff92a3f8d5f2112e8b4eddc0718
Calvin Lee <clee421@gmail.com>
2017-10-30 14:35:43 -0400

    JS Hoisting

---
+++ Javascript/03_hoisting.js
@@ -0,0 +1,32 @@
+function showName() {
+  console.log("First Name: " + name);
+  var name = "Ford";
+  console.log("Last Name: " + name);
+}
+
+showName();
+// First Name: undefined
+// Last Name: Ford
+
+// The reason undefined prints first is because the local variable name was hoisted to the top of the function
+// Which means it is this local variable that get calls the first time.
+// This is how the code is actually processed by the JavaScript engine:
+  
+function showName () {
+  var name; // name is hoisted (note that is undefined at this point, since the assignment happens below)
+  console.log ("First Name: " + name); // First Name: undefined
+
+  name = "Ford"; // name is assigned a value
+
+  // now name is Ford
+  console.log ("Last Name: " + name); // Last Name: Ford
+}
+
+ // Both the variable and the function are named myName
+ var myName;
+ function myName() {
+  console.log ("Rich");
+ }
+ 
+ // The function declaration overrides the variable name
+ console.log(typeof myName); // function
--- Javascript/notes.js
+++ Javascript/notes.js
@@ -1,2 +1,5 @@
 // Creating private variables in ES6
 // https://medium.com/@davidrhyswhite/private-members-in-es6-db1ccd6128a5
+
+// Use strict
+// https://stackoverflow.com/questions/1335851/what-does-use-strict-do-in-javascript-and-what-is-the-reasoning-behind-it
