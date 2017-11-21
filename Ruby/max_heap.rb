f9820674b52c9a965d6299b8687f2ba40c57c8ca
Calvin Lee <clee421@gmail.com>
2017-11-21 13:20:58 -0500

    Add comments

--- Ruby/max_heap.rb
+++ Ruby/max_heap.rb
@@ -10,45 +10,72 @@
 
 class MaxHeap
   def initialize
+    # Heap starts off empty
     @heap = []
   end
   
   def peek
+    # Checking the largest value
     @heap.first
   end
   
   def insert(val)
+    # Insert the value in the last position
+    # heapify up will put it where it needs to be
     @heap.push(val)
     heapify_up
   end
   
   def extract
+    # Grab the max value and hold onto it
     max = @heap.first
+
+    # Swap the first item with the last to pop
     @heap[0], @heap[-1] = @heap[-1], @heap[0]
     @heap.pop
+
+    # First item might be out of position so heapify down
     heapify_down
+
+    # Return the max value
     max
   end
   
   def show_heap
+    # Return a duplicate of the heap
+    # this is only a shallow copy so next items
+    # will need to be duped too
     @heap.dup
   end
   
   def size
+    # Size of heap
     @heap.length
   end
 
   def sort
+    # Sorting the heap in place modifies the heap
+    # Keep a dup of it first. It's ok for the
+    # dupe to be shallow since sorting is about order
     dupe = @heap.dup
+
+    # last_idx will keep track of where the last part of
+    # the heap is and where the sorted portion starts
     last_idx = @heap.length - 1
+
+    # Keep heapify'n until you're on the first item
     until last_idx <= 0
       swap(0, last_idx)
       last_idx -= 1
       heapify_down(0, last_idx)
     end
 
+    # The heap is sorted at this point so hold onto 
+    # the sorted heap and reassign @heap
     sort = @heap
     @heap = dupe
+
+    # Return the sorted heap
     sort
   end
   
@@ -89,6 +116,9 @@ class MaxHeap
   end
   
   def get_child_idx(p_idx, last_idx)
+    # get_child_idx takes in a parent index and finds the children
+    # index but only as far as last index permits since
+    # beyond that is where the sorted part will start
     base = (p_idx * 2)
     a = base + 1
     b = base + 2
@@ -98,6 +128,8 @@ class MaxHeap
   end
   
   def swap(idx_one, idx_two)
+    # Helper for swaping positions, this happens a lot and
+    # it's good to keep it easier to read
     @heap[idx_one], @heap[idx_two] = @heap[idx_two], @heap[idx_one]
   end
 end
