f389f79d50e468fa4fd53388c618c2d2316bcb13
Calvin Lee <clee421@gmail.com>
2017-11-27 14:00:12 -0500

    Pancake sort

---
+++ Go/pancake_sort.go
@@ -0,0 +1,58 @@
+package main
+
+import "fmt"
+
+func main() {
+	arr := []int{2, 4, 5, 3, 1}
+	fmt.Println(arr)
+
+	pancakeSort(&arr)
+	fmt.Println(arr)
+}
+
+func pancakeSort(arr *[]int) {
+	for last := len(*arr) - 1; last > 0; last-- {
+		index := largestByIndex(arr, last)
+
+		if index == last {
+			continue
+		}
+
+		if index != 0 {
+			flip(arr, index+1)
+		}
+
+		flip(arr, last+1)
+	}
+}
+
+func flip(arr *[]int, k int) {
+	if arr == nil || len(*arr) < 2 {
+		return
+	}
+
+	if k > len(*arr) {
+		k = len(*arr)
+	}
+
+	a := *arr
+	for i := 0; i < k/2; i++ {
+		a[i], a[k-1-i] = a[k-1-i], a[i]
+	}
+}
+
+func largestByIndex(arr *[]int, last int) int {
+	a := *arr
+
+	var li int
+	lv := a[0]
+
+	for i := 0; i <= last; i++ {
+		if a[i] > lv {
+			lv = a[i]
+			li = i
+		}
+	}
+
+	return li
+}
