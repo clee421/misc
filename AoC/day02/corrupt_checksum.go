d70232f0614fb53ecdc06a230aa2c23178273c83
Calvin Lee <clee421@gmail.com>
2017-12-04 20:26:04 -0500

    AOC Day2 Part2

--- AoC/day02/corrupt_checksum.go
+++ AoC/day02/corrupt_checksum.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"sort"
 	"strconv"
 	"strings"
 )
@@ -21,14 +22,16 @@ func main() {
 	scanner := bufio.NewScanner(f)
 	for scanner.Scan() {
 		line := strings.Split(scanner.Text(), "\t")
-		sum += getMinMaxDifference(line)
+		// sum += getMinMaxDifference(line)
+		sum += evenDivision(line)
 	}
 
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Printf("Checksum: %v\n", sum)
+	// fmt.Printf("Checksum: %v\n", sum)
+	fmt.Printf("Checksum (part 2): %v\n", sum)
 }
 
 func getMinMaxDifference(sa []string) int {
@@ -57,4 +60,27 @@ func getMinMaxDifference(sa []string) int {
 	}
 
 	return max - min
+}
+
+func evenDivision(sa []string) int {
+	na := make([]int, 0, len(sa))
+	for _, v := range sa {
+		num, err := strconv.Atoi(v)
+		if err != nil {
+			panic(err)
+		}
+
+		na = append(na, num)
+	}
+
+	sort.Ints(na)
+	for i := len(na) - 1; i >= 0; i-- {
+		for j := 0; j < i; j++ {
+			if na[i]%na[j] == 0 {
+				return na[i] / na[j]
+			}
+		}
+	}
+
+	return 0
 }
