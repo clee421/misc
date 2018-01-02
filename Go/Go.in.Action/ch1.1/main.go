8a3436e00d13ee6678d62771a35dc725ecc2bd9e
Calvin Lee <clee421@gmail.com>
2018-01-02 14:50:24 -0500

    Go In Action ch1.2

---
+++ Go/Go.in.Action/ch1.1/main.go
@@ -0,0 +1,13 @@
+// Go programs are organized as packages
+package main
+
+// The import statement allows you to use external code.
+// The fmt package from the standard library allows you to
+// format and output data
+import "fmt"
+
+// Just like in C, the main function is what gets
+// executed when the application is run
+func main() {
+	fmt.Println("Hello, World!")
+}
