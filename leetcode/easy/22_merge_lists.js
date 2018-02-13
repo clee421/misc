99fa7c6c8635762071e93df8badfec77e967a6ee
Calvin Lee <clee421@gmail.com>
2018-02-13 00:00:57 -0500

    leetcode-easy merge lists - fix

--- leetcode/easy/22_merge_lists.js
+++ leetcode/easy/22_merge_lists.js
@@ -11,37 +11,38 @@
  * @return {ListNode}
  */
 var mergeTwoLists = function(l1, l2) {
-    let head = null
-    let current = null
-    
-    while(l1 !== null && l2 !== null) {
-        let temp
-        if (l1.val < l2.val) {
-            temp = l1
-            l1 = l1.next
-        } else {
-            temp = l2
-            l2 = l2.next
-        }
-        
-        if (head === null) {
-            head = new ListNode(temp.val)
-            current = head
-        } else {
-            current.next = new ListNode(temp.val)
-            current = current.next
-        }
-    }
-    
-    let leftOver = null
-    if (l1 !== null || l2 !== null) {
-        leftOver = l1 !== null ? l1 : l2
-    }
-    
-    while (leftOver !== null) {
-        current.next = new ListNode(leftOver.val)
-        current = current.next
-    }
-    
-    return head
+  let head = null
+  let current = null
+  
+  while(l1 !== null && l2 !== null) {
+      let temp
+      if (l1.val < l2.val) {
+          temp = l1
+          l1 = l1.next
+      } else {
+          temp = l2
+          l2 = l2.next
+      }
+      
+      if (head === null) {
+          head = new ListNode(temp.val)
+          current = head
+      } else {
+          current.next = new ListNode(temp.val)
+          current = current.next
+      }
+  }
+  
+  let leftOver = null
+  if (l1 !== null || l2 !== null) {
+      leftOver = l1 !== null ? l1 : l2
+  }
+  
+  while (leftOver !== null) {
+      current.next = new ListNode(leftOver.val)
+      current = current.next
+      leftOver = leftOver.next
+  }
+  
+  return head
 };
