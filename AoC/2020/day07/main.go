c0670ce5b812cff9823dfc211328ec58b7a8cc5e
Calvin Lee <clee421@gmail.com>
2020-12-12 18:31:08 -0500

    aoc: 2020 d7 part1 part2

--- AoC/2020/day07/main.go
+++ AoC/2020/day07/main.go
@@ -16,15 +16,92 @@ type BagRule struct {
 	}
 }
 
-func main() {
-	r := readFile("./test.data")
-	rules := parseData(r)
-
-	fmt.Println(rules)
+// BagNode keeps track of the child to parent relationship
+type BagNode struct {
+	Color   string
+	Parents []string
 }
 
-func parseData(d []string) []BagRule {
-	rules := []BagRule{}
+func main() {
+	r := readFile("./input.data")
+	rules := parseData(r)
+	parents := buildParentGraph(rules)
+	count := countParentsHierachy(parents, "shiny gold")
+
+	fmt.Println("Parent count for shiny gold bag is:", count)
+
+	count = countChildrenHierachy(rules, "shiny gold")
+	fmt.Println("Child count for shiny gold bag is:", count)
+}
+
+func countChildrenHierachy(g map[string]*BagRule, start string) int {
+	type Rule struct {
+		Color string
+		Count int
+	}
+
+	sum := 0
+	queue := []Rule{Rule{start, 1}}
+
+	for len(queue) > 0 {
+		rule := queue[0]
+		queue = queue[1:]
+
+		if node, ok := g[rule.Color]; ok {
+			// fmt.Println("Current color", rule.Color)
+			for _, c := range node.Contains {
+				// fmt.Printf("Has %d x %d %s bag(s)\n", rule.Count, c.Count, c.Color)
+				count := rule.Count * c.Count
+				sum += count
+
+				// fmt.Println("Sum:", sum)
+				queue = append(queue, Rule{c.Color, (rule.Count * c.Count)})
+			}
+		}
+	}
+
+	return sum
+}
+
+func countParentsHierachy(g map[string]*BagNode, target string) int {
+	seen := map[string]bool{}
+	queue := []string{target}
+
+	for len(queue) > 0 {
+		color := queue[0]
+		queue = queue[1:]
+
+		// fmt.Println("My target is:", color)
+		if node, ok := g[color]; ok {
+			// fmt.Println("Has parents:", node.Parents)
+
+			for _, p := range node.Parents {
+				seen[p] = true
+				queue = append(queue, p)
+			}
+		}
+	}
+
+	return len(seen)
+}
+
+func buildParentGraph(rules map[string]*BagRule) map[string]*BagNode {
+	graph := map[string]*BagNode{}
+	for _, r := range rules {
+		for _, child := range r.Contains {
+			if _, ok := graph[child.Color]; !ok {
+				graph[child.Color] = &BagNode{child.Color, []string{}}
+			}
+
+			graph[child.Color].Parents = append(graph[child.Color].Parents, r.Color)
+		}
+	}
+
+	return graph
+}
+
+func parseData(d []string) map[string]*BagRule {
+	rules := map[string]*BagRule{}
 
 	for _, l := range d {
 		rule := BagRule{}
@@ -35,7 +112,7 @@ func parseData(d []string) []BagRule {
 		// fmt.Println("Color:", rule.Color)
 
 		if s[1] == "no other bags." {
-			rules = append(rules, rule)
+			rules[rule.Color] = &rule
 			continue
 		}
 
@@ -57,7 +134,7 @@ func parseData(d []string) []BagRule {
 				Count int
 			}{color, n})
 
-			rules = append(rules, rule)
+			rules[rule.Color] = &rule
 			// fmt.Println("  Contains:", n, color)
 		}
 	}
