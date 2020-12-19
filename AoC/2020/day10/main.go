aeb5b7387344c29f8c81208d0e98aaaabe89f6c8
Calvin Lee <clee421@gmail.com>
2020-12-19 16:11:56 -0500

    aoc: 2020 d10 part2; just dont implement your own stack

--- AoC/2020/day10/main.go
+++ AoC/2020/day10/main.go
@@ -14,6 +14,61 @@ func main() {
 
 	joltDiff := calculateJoltDiff(nums)
 	fmt.Println("Jolt diff:", joltDiff)
+
+	tree := buildTree(nums)
+	result := calculateCombinations(tree, nums[len(nums)-1])
+	fmt.Println("Jolt combos:", result)
+}
+
+func buildTree(sortedNums []int) map[int][]int {
+	l := len(sortedNums)
+
+	tree := map[int][]int{}
+	curr := 0
+
+	for i, n := range sortedNums {
+		check := []int{}
+		for s := i; s < l; s++ {
+			if sortedNums[s] <= curr+3 {
+				check = append(check, sortedNums[s])
+			}
+
+			if sortedNums[s] > curr+3 {
+				break
+			}
+		}
+
+		tree[curr] = check
+		curr = n
+	}
+
+	return tree
+}
+
+func calculateCombinations(tree map[int][]int, last int) int {
+	memo := map[int]*int{}
+	return dfs(tree, 0, last, memo)
+}
+
+// I attempted to implement my own stack to use for the DFS. It turned out to be terrible and
+// you should never do that. There's much more to handle than just the value.
+func dfs(tree map[int][]int, start int, last int, memo map[int]*int) int {
+	if memo[start] != nil {
+		return *memo[start]
+	}
+
+	if start == last {
+		return 1
+	}
+
+	sum := 0
+	for _, n := range tree[start] {
+		sum += dfs(tree, n, last, memo)
+	}
+
+	memo[start] = &sum
+
+	return sum
 }
 
 func calculateJoltDiff(sortedNums []int) int {
