67f47dd4e1a4ef6a7fa709bcafce6e194a2e29c3
Calvin Lee <clee421@gmail.com>
2020-12-20 16:37:26 -0500

    aoc: 2020 d12 part2 :sob: im not smart enough to not write tests... so many bugs :sob:

--- AoC/2020/day12/main.go
+++ AoC/2020/day12/main.go
@@ -20,6 +20,13 @@ type Ship struct {
 	north     int
 }
 
+// Waypoint waypoint
+type Waypoint struct {
+	ship  Ship
+	east  int
+	north int
+}
+
 func (s *Ship) distance() int {
 	return abs(s.east) + abs(s.north)
 }
@@ -76,12 +83,144 @@ func (s *Ship) update(direction byte, degrees int) {
 	s.direction = order[nd]
 }
 
+func (w *Waypoint) run(in Instruction) {
+	switch in.action {
+	case 'N':
+		w.north += in.value
+	case 'S':
+		w.north -= in.value
+	case 'E':
+		w.east += in.value
+	case 'W':
+		w.east -= in.value
+	case 'F':
+		w.moveShip(in.value)
+	case 'L':
+		w.rotateCounterClockwise(in.value)
+	case 'R':
+		w.rotateClockwise(in.value)
+	default:
+		panic(1)
+	}
+}
+
+func (w *Waypoint) getDirections() struct {
+	north int
+	south int
+	east  int
+	west  int
+} {
+	points := struct {
+		north int
+		south int
+		east  int
+		west  int
+	}{}
+
+	if w.north > 0 {
+		points.north = w.north
+	}
+
+	if w.north < 0 {
+		points.south = -w.north
+	}
+
+	if w.east > 0 {
+		points.east = w.east
+	}
+
+	if w.east < 0 {
+		points.west = -w.east
+	}
+
+	return points
+}
+
+func (w *Waypoint) setDirections(points struct {
+	north int
+	south int
+	east  int
+	west  int
+}) {
+	w.north = points.north - points.south
+	w.east = points.east - points.west
+}
+
+func (w *Waypoint) rotateClockwise(degrees int) {
+	d := w.getDirections()
+
+	// yea this could have just been math but.. i'm lazy
+	rotations := degrees / 90
+	for i := 0; i < rotations; i++ {
+		tmp := d.east
+		d.east = d.north
+		d.north = d.west
+		d.west = d.south
+		d.south = tmp
+	}
+
+	w.setDirections(d)
+}
+
+func (w *Waypoint) rotateCounterClockwise(degrees int) {
+	d := w.getDirections()
+
+	rotations := degrees / 90
+	for i := 0; i < rotations; i++ {
+		tmp := d.west
+		d.west = d.north
+		d.north = d.east
+		d.east = d.south
+		d.south = tmp
+	}
+
+	w.setDirections(d)
+}
+
+func (w *Waypoint) moveShip(times int) {
+	for i := 0; i < times; i++ {
+		var y byte = 'N'
+		if w.north < 0 {
+			y = 'S'
+		}
+
+		var x byte = 'E'
+		if w.east < 0 {
+			x = 'W'
+		}
+		w.ship.move(Instruction{action: y, value: abs(w.north)})
+		w.ship.move(Instruction{action: x, value: abs(w.east)})
+	}
+}
+
+func (w *Waypoint) distance() int {
+	return w.ship.distance()
+}
+
 func main() {
 	r := readFile("./input.data")
 	ins := parseInstructions(r)
 
 	result1 := solve1(ins)
-	fmt.Println("Total distance:", result1)
+	fmt.Println("Total distance solve1:", result1)
+
+	result2 := solve2(ins)
+	fmt.Println("Total distance solve2:", result2)
+}
+
+func solve2(instructions []Instruction) int {
+	ship := Ship{direction: 'E'}
+	wp := Waypoint{ship, 10, 1}
+	for i, in := range instructions {
+		wp.run(in)
+
+		fmt.Println(i, "current value", wp.distance())
+		fmt.Println(wp.east, wp.north)
+	}
+
+	// fmt.Println(ship.north, ship.east)
+
+	return wp.distance()
 }
 
 func solve1(instructions []Instruction) int {
